014f867e55c6c52690f7cf430553f9588694bb9e
Jorra <core9500@mail.ru>
2021-04-11 20:13:54 +0300

    refactor: fix code

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true, useFindAn
   
 server.listen(port, ()=>{console.log(`listening on port ${port}`)});
 
-const users = {}
-const room = {
+let users = {}
+let room = {
   admin: null,
   size: 6,
   customWords: false,
@@ -46,12 +46,11 @@ const room = {
   teamTurn: 'red',
   teamWordsClicked: [],
   timeBeforeOpenCard: 1500,
-  wordsChoosed: [],
   redWordsLeft: 12,
   blueWordsLeft: 12,
   greenWordsLeft: 12,
 }
-
+let timeoutID;
 let wordChosen = null;
 let globaleTime = {timeToThinkLeft: 60, timeToAnswerLeft: 60, MasterTime: true, MasterTurn: true}
 let arrayToPlay = []
@@ -92,10 +91,11 @@ const timer = () => {
       }
     }
     io.emit('globaleTime', globaleTime);
+    !room.gameStarted && clearInterval(timer);
   }, 1000)
 }
 
-app.post('/getPort', async (req, res) => {
+app.get('/getPort', async (req, res) => {
   res.status(200).json({ port: process.env.PORT })
 })
 
@@ -118,17 +118,50 @@ app.post('/addWord', async (req, res) => {
   }
 })
 
+const roomNeedForAdmin = () => {
+  const arraysEmpty = 
+  !!room.red[0]
+    || !!room.blue[0]
+    || !!room.green[0]
+    || !!room.white[0]
+    || !!room.redMaster[0]
+    || !!room.blueMaster[0]
+    || !!room.greenMaster[0]
+      ? false
+      : true
+  return arraysEmpty
+}
 
-const setUsername = (client,username) => {
-  const needAdmin = (!!room.red[0] || !!room.blue[0] || !!room.green[0] || !!room.white[0] || !!room.redMaster[0] || !!room.blueMaster[0] || !!room.greenMaster[0] ? false : true) ;
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
+
+const normalDate = () => {
+  function addZero (date) {
+    if(String(date).length === 1) {
+      return `0${date}`
+    } else {
+      return date
+    }
+  }
+  const Fulldate = new Date();
+  const date = addZero(Fulldate.getDate());
+  const mount = addZero(Fulldate.getMonth()+1);
+  const year = addZero(Fulldate.getFullYear());
+  const hour = addZero(Fulldate.getHours());
+  const minute = addZero(Fulldate.getMinutes());
+  const second = addZero(Fulldate.getSeconds());
+  return date+'.'+mount+'.'+year+' в '+hour+':'+minute+':'+second;
+}
+
+const setUsername = (client, username) => {
+  console.log(`${username} присоеденился к серверу ${normalDate()};`);
+  const needAdmin = roomNeedForAdmin() ;
   const user = {
     id: client.id,
     name: username,
@@ -137,7 +170,6 @@ const setUsername = (client,username) => {
     personalColors: getRandomColor(),
     team: 'white'
   }
-  console.log(`${username} ${client.handshake.address.replace('::ffff:', '')} присоеденился к серверу в ${new Date()}`);
   users[client.id] = user;
   room.white.push(user);
   if(needAdmin){
@@ -151,91 +183,96 @@ const setUsername = (client,username) => {
 }
 
 const startGame = (client) => {
-if(!room.gameStarted && client.id === room.admin && room.gotWords){
-  if(room.red.length > 0 && room.blue.length > 0 && room.redMaster.length > 0 && room.blueMaster.length > 0){
-    room.gameStarted = true;
-    io.emit('room', room);
-    timer();
+  if(!room.gameStarted && client.id === room.admin && room.gotWords){
+    if(room.red.length > 0 && room.blue.length > 0 && room.redMaster.length > 0 && room.blueMaster.length > 0){
+      room.gameStarted = true;
+      io.emit('room', room);
+      timer();
+    }
   }
-}
 }
 
 const becomePlayer = (client,newTeam) => {
-if(!room.gameStarted && (users[client.id])){
-  const myTeam = (users[client.id])['team'];
-  if (myTeam !== newTeam || (users[client.id])['isMaster']) {
-    (users[client.id])['team'] = newTeam;
-    (users[client.id])['isMaster'] = false;
-    room[newTeam].push(users[client.id]);
-    let myIndex = null
-    if(myTeam === 'white') {
-      myIndex = room[myTeam].findIndex(n => n.id === client.id);
-    } else {
-      myIndex = room[`${myTeam}Master`].findIndex(n => n.id === client.id);
+  if(!room.gameStarted && (users[client.id])){
+    const myTeam = (users[client.id])['team'];
+    if (myTeam !== newTeam || (users[client.id])['isMaster']) {
+      (users[client.id])['team'] = newTeam;
+      (users[client.id])['isMaster'] = false;
+      room[newTeam].push(users[client.id]);
+      let myIndex = null
+      if(myTeam === 'white') {
+        myIndex = room[myTeam].findIndex(n => n.id === client.id);
+      } else {
+        myIndex = room[`${myTeam}Master`].findIndex(n => n.id === client.id);
+      }
+      if (myIndex !== -1 && myTeam !== 'white') {
+        room[`${myTeam}Master`] = [];
+      } else {
+        room[myTeam].splice(myIndex, 1);
+      }
+      if(room.gotWords) {
+        io.to(client.id).emit('words', arrayToPlay)
+      }
+      io.emit('room', room);
     }
-    if (myIndex !== -1 && myTeam !== 'white') {
-      room[`${myTeam}Master`] = [];
-    } else {
-      room[myTeam].splice(myIndex, 1);
-    }
-    if(room.gotWords) {
-      io.to(client.id).emit('words', arrayToPlay)
-    }
-    io.emit('room', room);
   }
-}
 }
 
 const becomeMaster = (client,newTeam) => {
-if(!room.gameStarted){
-  const myTeam = (users[client.id])['team']
-  if (!room[`${newTeam}Master`].length) {
-    (users[client.id])['team'] = newTeam;
-    (users[client.id])['isMaster'] = true;
-    room[`${newTeam}Master`].push(users[client.id]);
-    const myIndex = room[myTeam].findIndex(n => n.id === client.id);
-    if (myIndex !== -1) {
-      room[myTeam].splice(myIndex, 1);
-    } else {
-      room[`${myTeam}Master`] = [];
+  if(!room.gameStarted){
+    const myTeam = (users[client.id])['team']
+    if (!room[`${newTeam}Master`].length) {
+      (users[client.id])['team'] = newTeam;
+      (users[client.id])['isMaster'] = true;
+      room[`${newTeam}Master`].push(users[client.id]);
+      const myIndex = room[myTeam].findIndex(n => n.id === client.id);
+      if (myIndex !== -1) {
+        room[myTeam].splice(myIndex, 1);
+      } else {
+        room[`${myTeam}Master`] = [];
+      }
+      if(room.gotWords) {
+        io.to(client.id).emit('words', arrayToMaster)
+      }
+      io.emit('room', room);
     }
-    if(room.gotWords) {
-      io.to(client.id).emit('words', arrayToMaster)
-    }
-    io.emit('room', room);
   }
 }
+
+const createWordList = async () => {
+  const currentScema = mongoose.model('words',wordShema);
+  const randomWords = await currentScema.aggregate([{ $sample: { size: room.size*room.size } },{ $match: { custom: room.customWords} }])
+  arrayToPlay = [];
+  arrayToMaster = [];
+  randomWords.map((word,i) => arrayToMaster.push({...word, isClicked: false, team: i === 0 ? 'black' : i<=2*room.size ? 'red' : i<=4*room.size ? 'blue' : 'white'}));
+  arrayToMaster.sort((a,b)=>Math.random() - 0.5)
+  arrayToMaster.map((word,i) => arrayToPlay.push({...word, team: 'white'}));
 }
 
 const newWords = async (client) => {
   if(!room.gameStarted && client.id === room.admin){
-    const currentScema = mongoose.model('words',wordShema);
-    const randomWords = await currentScema.aggregate([{ $sample: { size: room.size*room.size } },{ $match: { custom: room.customWords} }])
-    arrayToPlay = [];
-    arrayToMaster = [];
-    randomWords.map((word,i) => arrayToMaster.push({...word, isClicked: false, team: i === 0 ? 'black' : i<=2*room.size ? 'red' : i<=4*room.size ? 'blue' : 'white'}));
-    arrayToMaster.sort((a,b)=>Math.random() - 0.5)
-    arrayToMaster.map((word,i) => arrayToPlay.push({...word, team: 'white'}));
-    room.redWordsLeft = 2*room.size;
-    room.blueWordsLeft = 2*room.size;
-    room.greenWordsLeft = 2*room.size;
-    room.gameOver = false;
-    room.wordsChoosed = [];
-    globaleTime.timeToThinkLeft = room.timeToThink;
-    globaleTime.timeToAnswerLeft = room.timeToAnswer;
-    room.redWords = [];
-    room.blueWords = [];
-    room.greenWords = [];
-    globaleTime.MasterTime = true;
-    globaleTime.MasterTurn = true;
-
-    if(arrayToMaster.length === room.size*room.size){
-      Object.keys(users).map(
-        user => io.to(user).emit('words', (users[user])['isMaster'] ? arrayToMaster : arrayToPlay)
-      );
-      room.gotWords = true;
-      io.emit('room', room);
+    await createWordList();
+    console.log(arrayToMaster.length)
+    room = {...room,
+      redWordsLeft: 2*room.size,
+      blueWordsLeft: 2*room.size,
+      greenWordsLeft: 2*room.size,
+      gotWords: true,
+      redWords: [],
+      blueWords: [],
+      greenWords: []
     }
+    globaleTime = {...globaleTime,
+      timeToThinkLeft: room.timeToThink,
+      timeToAnswerLeft: room.timeToAnswer,
+      MasterTime: true,
+      MasterTurn: true
+    }
+    console.log('newWords send')
+    Object.keys(users).map(
+      user => io.to(user).emit('words', (users[user])['isMaster'] ? arrayToMaster : arrayToPlay)
+    );
+    io.emit('room', room);
   }
 }
 
@@ -249,6 +286,7 @@ const pauseGame = (client) => {
 const sendMessage = (client, message) => {
   globaleTime.MasterTurn = false;
   room[`${(users[client.id])['team']}Words`].push(message);
+  io.emit('globaleTime', globaleTime);
   io.emit('room', room);
 }
 
@@ -267,67 +305,89 @@ const changeSettings = (client,prop, value) => {
   }
 }
 
-const clickButton = (client, buttonIndex) => {
+const openCard = (buttonIndex) => {
+  if(wordChosen !== null) {
+    arrayToMaster[buttonIndex].isClicked = true;
+    arrayToPlay[buttonIndex] = arrayToMaster[buttonIndex];
+    if(arrayToMaster[buttonIndex].team === room.teamTurn){
+      room[`${arrayToMaster[buttonIndex].team}WordsLeft`]--;
+      globaleTime.timeToAnswerLeft+=room.timeByCorrectAnswer;
+    } else if(arrayToMaster[buttonIndex].team === 'black') {
+      gameOver();
+    } else {
+      room[`${arrayToMaster[buttonIndex].team}WordsLeft`]--;
+      room = {...room,
+        teamTurn: room.teamTurn === 'red' ? 'blue' : 'red',
+        teamWordsClicked: []
+      }
+      globaleTime = {...globaleTime,
+        MasterTurn: true,
+        MasterTime: true,
+        timeToThinkLeft: room.timeToThink,
+        timeToAnswerLeft: room.timeToAnswer
+      }
+    }
+    if(!room.redWordsLeft || !room.blueWordsLeft || !room.greenWordsLeft) {
+      gameOver();
+    }
+    io.emit('room', room);
+    if(room[`${arrayToMaster[buttonIndex].team}WordsLeft`] === 0) {
+      room = {...room,
+        gameStarted: false,
+        gotWords:false
+      }
+      arrayToPlay = arrayToMaster;
+    }
+    console.log('click send')
+    Object.keys(users).map(
+      user => io.to(user).emit('words', (users[user])['isMaster'] ? arrayToMaster : arrayToPlay)
+    )
+  }
+}
+
+const addChosenWord = (client,buttonIndex) => {
+  const index = room.teamWordsClicked.findIndex(n => n.user === client.id);
+  if (index === -1) {
+    room.teamWordsClicked.push({user: client.id, color: users[client.id].personalColors, id: buttonIndex})
+  } else {
+    const bi = room.teamWordsClicked[index].id;
+    room.teamWordsClicked.splice(index, 1);
+    if(bi !== buttonIndex) {
+      room.teamWordsClicked.push({user: client.id, color: users[client.id].personalColors, id: buttonIndex})
+    }
+  }
+}
+
+const chosenWordCorrect = () => {
+  if(room.teamWordsClicked.length === room[room.teamTurn].length ||
+    room[room.teamTurn].length === 1){
+    for (let i = 0; i < room.teamWordsClicked.length - 1; i++) {
+      if(room.teamWordsClicked[i].id !== room.teamWordsClicked[i + 1].id) {
+        console.log(1)
+        return false;
+      }
+    }
+  } else {
+    console.log(2)
+    return false;
+  }
+  return true
+}
+
+const clickButton = (client, buttonIndex, time) => {
   if(!(users[client.id])['isMaster']) {
     if(room.teamTurn === (users[client.id])['team'] && !arrayToPlay[buttonIndex].isClicked && room.gameStarted && !globaleTime.MasterTurn) {
-      const index = room.teamWordsClicked.findIndex(n => n.user === client.id);
-      if (index === -1) {
-        room.teamWordsClicked.push({user: client.id, color: users[client.id].personalColors, id: buttonIndex})
-      } else {
-        const bi = room.teamWordsClicked[index].id;
-        room.teamWordsClicked.splice(index, 1);
-        if(bi !== buttonIndex) {
-          room.teamWordsClicked.push({user: client.id, color: users[client.id].personalColors, id: buttonIndex})
-        }
+      if(room[room.teamTurn].length > 1) {
+        addChosenWord(client,buttonIndex);
       }
-      wordChosen = buttonIndex;
-      if(room.teamWordsClicked.length === room[room.teamTurn].length){
-        for (let i = 0; i < room.teamWordsClicked.length - 1; i++) {
-          if(room.teamWordsClicked[i].id !== room.teamWordsClicked[i + 1].id) {
-            wordChosen = null;
-          }
-        }
-      } else {
-        wordChosen = null;
-      }
-      var timeoutID;
+      wordChosen = chosenWordCorrect() ? buttonIndex : null;
       io.emit('wordChosen', wordChosen);
       if(wordChosen !== null){
-        timeoutID = setTimeout(openCard, 2000);
-        console.log('create')
-        function openCard() {
-          if(wordChosen !== null) {
-            wordChosen = null;
-            arrayToMaster[buttonIndex].isClicked = true;
-            arrayToPlay[buttonIndex].isClicked = true;
-            arrayToPlay[buttonIndex].team = arrayToMaster[buttonIndex].team === 'white' ? 'openedWhite' : arrayToMaster[buttonIndex].team;
-            if(arrayToMaster[buttonIndex].team === room.teamTurn){
-              room[`${arrayToMaster[buttonIndex].team}WordsLeft`]--;
-              globaleTime.timeToAnswerLeft+=room.timeByCorrectAnswer;
-            } else if(arrayToMaster[buttonIndex].team === 'black') {
-              room.gameStarted = false;
-              room.gotWords = false;
-              arrayToPlay = arrayToMaster;
-            } else {
-              room.teamTurn = (room.teamTurn === 'red' ? 'blue' : 'red');
-              room[`${arrayToMaster[buttonIndex].team}WordsLeft`]--;
-              globaleTime.MasterTurn = true;
-              globaleTime.MasterTime = true;
-              globaleTime.timeToThinkLeft = room.timeToThink;
-              globaleTime.timeToAnswerLeft = room.timeToAnswer;
-            }
-            room.teamWordsClicked = [];
-            io.emit('room', room);
-            if(room[`${arrayToMaster[buttonIndex].team}WordsLeft`] === 0) {
-              room.gameStarted = false;
-              room.gotWords = false;
-              arrayToPlay = arrayToMaster;
-            }
-            Object.keys(users).map(
-              user => io.to(user).emit('words', (users[user])['isMaster'] ? arrayToMaster : arrayToPlay)
-              )
-            }
-          }
+        clearTimeout(timeoutID);
+        const date = new Date();
+        const localTimeOffset = date.getTimezoneOffset() * 60000;
+        const offset = date.getTime() - localTimeOffset - time;
+        timeoutID = setTimeout(()=>openCard(buttonIndex), 1000 + offset);
         } else {
           clearTimeout(timeoutID)
           io.emit('room', room);
@@ -338,11 +398,17 @@ const clickButton = (client, buttonIndex) => {
   }
 }
 
+const gameOver = () => {
+  room.gameStarted = false;
+  room.gotWords = false;
+  arrayToPlay = arrayToMaster;
+}
+
 const disconnect = (client) => {
   if(users[client.id]) {
     const team = (users[client.id])['team'];
     const index = room[team].findIndex(n => n.id === client.id);
-    console.log(`${(users[client.id])['name']} отключился в ${new Date()}`);
+    console.log(`${(users[client.id])['name']} отключился ${normalDate()};`);
     if (index !== -1) {
       room[team].splice(index, 1);
     } else {
@@ -350,29 +416,29 @@ const disconnect = (client) => {
     }
     delete users[client.id];
     room.admin = Object.values(users)[0] ? (Object.values(users)[0]).id : null;
-    io.emit('room', room);
   }
+  io.emit('room', room);
 }
 
 io.on('connection', client => {
   client.on('username', username => {
-    console.log('username')
-    setUsername(client,username)
+    console.log('username');
+    setUsername(client,username);
   })
 
   client.on('becomePlayer', newTeam => {
-    console.log('becomePlayer')
-    becomePlayer(client,newTeam)
+    console.log('becomePlayer');
+    becomePlayer(client,newTeam);
   })
 
   client.on('becomeMaster', newTeam => {
-    console.log('becomeMaster')
-    becomeMaster(client,newTeam)
+    console.log('becomeMaster');
+    becomeMaster(client,newTeam);
   })
 
   client.on('newWords', () => {
-    console.log('newWords')
-    newWords(client)
+    console.log('newWords');
+    newWords(client);
   })
 
   client.on('startGame', () => {
@@ -381,27 +447,28 @@ io.on('connection', client => {
   })
 
   client.on('pauseGame', () => {
-    console.log('pauseGame')
-    pauseGame(client)
+    console.log('pauseGame');
+    pauseGame(client);
   })
 
   client.on('send', message => {
-    console.log('send')
-    sendMessage(client,message)
+    console.log('send');
+    sendMessage(client,message);
   })
 
   client.on('settings', (prop, value) => {
-    console.log('settings')
-    changeSettings(client, prop, value)
+    console.log('settings');
+    changeSettings(client, prop, value);
   })
 
-  client.on('clickButton', buttonIndex => {
-    console.log('clickButton')
-    clickButton(client, buttonIndex)
+  client.on('clickButton', (buttonIndex, time) => {
+    console.log('clickButton');
+    clickButton(client, buttonIndex, time);
   })
 
   client.on('disconnect', () => {
-    console.log('disconnect')
-    disconnect(client)
+    console.log('disconnect');
+    room.gameStarted && gameOver();
+    disconnect(client);
   })
 })
